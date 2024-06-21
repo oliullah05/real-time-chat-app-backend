aca5c147ff589bdfc188c636abdd737652141725
oliullah05 <mdolihasan@gmail.com>
2024-06-21 13:00:51 +0600

    in message mdoel fileName property added

--- src/app/modules/message/message.validation.ts
+++ src/app/modules/message/message.validation.ts
@@ -2,6 +2,7 @@ import z from "zod"
 const createMessage = z.object({
     body: z.object({
         message: z.string({ required_error: "Message is required" }).min(1, { message: 'Message must not be empty' }),
+        fileName: z.string().min(1, { message: 'fileName must not be empty' }).optional(),
         conversationId: z.string({ required_error: "ConversationId is required" }),
         type: z.enum([
             'web',
